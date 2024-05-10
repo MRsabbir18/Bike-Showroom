97ba001457b96cbf747b07739ffec7a11e37f6e6
MRsabbir18 <sabbirhossain6757@gmail.com>
2024-05-11 01:17:09 +0600

    declare class

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Bike {
+    private String brand;
+    private String model;
+    private double price;
+
+    public Bike(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
